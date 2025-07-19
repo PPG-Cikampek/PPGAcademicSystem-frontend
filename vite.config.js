49000e8ec99c52a388663310906701fe9bd9c2db
MBrillian354 <brillian354@gmail.com>
2025-07-19 15:31:31 +0700

    Use the new React Compiler

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig, defaultClientConditions, defaultServerConditions } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    ['babel-plugin-react-compiler', { target: '18' }],
     react(),
     // mkcert()
   ],
