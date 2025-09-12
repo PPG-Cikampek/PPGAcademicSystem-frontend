16b3f836b9ee0a21c298386a73416329fdc0701a
MBrillian354 <brillian354@gmail.com>
2025-09-12 13:34:51 +0700

    Add fields and improve layout in RequestAccountForm

--- src/users/pages/RequestAccountForm.jsx
+++ src/users/pages/RequestAccountForm.jsx
@@ -44,6 +44,44 @@ const RequestAccountForm = () => {
             type: "email",
             required: true,
         },
+        {
+            name: "phone",
+            label: "Nomor WA Aktif",
+            placeholder: "8123456789",
+            type: "tel",
+            required: true,
+        },
+        {
+            name: "position",
+            label: "Posisi",
+            placeholder: "Pilih Posisi",
+            type: "select",
+            required: true,
+            options: [
+                { label: "MT Desa", value: "branchTeacher" },
+                { label: "MT Kelompok", value: "subBranchTeacher" },
+                { label: "MS", value: "localTeacher" },
+                { label: "Asisten", value: "assistant" },
+            ],
+        },
+        {
+            name: "gender",
+            label: "Jenis Kelamin",
+            type: "select",
+            placeholder: "Pilih Jenis Kelamin",
+            required: true,
+            options: [
+                { label: "Laki-Laki", value: "male" },
+                { label: "Perempuan", value: "female" },
+            ],
+        },
+        {
+            name: "address",
+            label: "Alamat",
+            placeholder: "Alamat Lengkap",
+            type: "textarea",
+            required: true,
+        },
     ];
 
     const studentFields = [
@@ -77,6 +115,38 @@ const RequestAccountForm = () => {
                 { label: "Kelas 5", value: "Kelas 5" },
                 { label: "Kelas 6", value: "Kelas 6" },
             ],
+        },
+        {
+            name: "gender",
+            label: "Jenis Kelamin",
+            type: "select",
+            placeholder: "Pilih Jenis Kelamin",
+            required: true,
+            options: [
+                { label: "Laki-Laki", value: "male" },
+                { label: "Perempuan", value: "female" },
+            ],
+        },
+        {
+            name: "parentName",
+            label: "Nama Orang Tua/Wali",
+            placeholder: "Nama Orang Tua/Wali",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "parentPhone",
+            label: "Nomor WA Orang Tua/Wali",
+            placeholder: "8123456789",
+            type: "tel",
+            required: true,
+        },
+        {
+            name: "address",
+            label: "Alamat",
+            placeholder: "Alamat Lengkap",
+            type: "textarea",
+            required: true,
         },
     ];
 
@@ -154,109 +224,137 @@ const RequestAccountForm = () => {
     const fields = isStudent ? studentFields : teacherFields;
 
     return (
-        <>
+        <div className="max-w-6xl mx-auto flex flex-col items-center-safe">
             <NewModal
                 modalState={modalState}
                 onClose={closeModal}
                 isLoading={isLoading}
             />
             {error && (
-                <div className="mx-2 mt-12">
+                <div className="mx-2 mt-12 w-full">
                     <ErrorCard error={error} onClear={() => setError(null)} />
                 </div>
             )}
-            <div className="card-basic rounded-md items-stretch flex-col m-2">
-                <div className="pb-24 transition-opacity duration-300">
-                    <div className="flex flex-col p-2 justify-center items-center my-6">
-                        <h2 className="text-2xl mt-4 font-medium text-center">
+
+            <div className="flex flex-col lg:flex-row w-full gap-6 px-2 mt-10">
+                
+                <div className="card-basic rounded-md flex flex-col w-full lg:basis-2/5">
+                    <div className="p-2 flex flex-col justify-center items-center mb-4 mt-2">
+                        <h2 className="text-2xl font-medium text-center">
                             {isStudent
                                 ? "Peserta Didik Baru"
                                 : "Tenaga Pendidik Baru"}
                         </h2>
                     </div>
-                    <form className="flex flex-col md:flex-row gap-4 items-end mb-12">
-                        {fields.map((field) => (
-                            <div
-                                key={field.name}
-                                className="flex flex-col w-full"
-                            >
-                                <label
-                                    htmlFor={field.name}
-                                    className="block text-gray-700 pb-1"
+                    <form className="flex flex-col gap-6 items-stretch mb-6">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+                            {fields.map((field) => (
+                                <div
+                                    key={field.name}
+                                    className={`flex flex-col w-full ${
+                                        field.name === "address"
+                                            ? "md:col-span-2"
+                                            : ""
+                                    }`}
                                 >
-                                    {field.label}
-                                </label>
-                                {field.type === "select" ? (
-                                    <select
-                                        id={field.name}
-                                        name={field.name}
-                                        value={formData[field.name] || ""}
-                                        onChange={handleInputChange}
-                                        required={field.required}
-                                        className="w-full p-2 border rounded-md shadow-xs hover:ring-secondary-subtle focus:outline-hidden focus:ring-2 focus:ring-secondary transition-all duration-300"
+                                    <label
+                                        htmlFor={field.name}
+                                        className="block text-gray-700 pb-1"
                                     >
-                                        <option value="" disabled>
-                                            {field.placeholder}
-                                        </option>
-                                        {field.options.map((option) => (
-                                            <option
-                                                key={option.value}
-                                                value={option.value}
-                                            >
-                                                {option.label}
+                                        {field.label}
+                                    </label>
+                                    {field.type === "select" ? (
+                                        <select
+                                            id={field.name}
+                                            name={field.name}
+                                            value={formData[field.name] || ""}
+                                            onChange={handleInputChange}
+                                            required={field.required}
+                                            className="w-full p-2 border rounded-md shadow-xs hover:ring-secondary-subtle focus:outline-hidden focus:ring-2 focus:ring-secondary transition-all duration-300"
+                                        >
+                                            <option value="" disabled>
+                                                {field.placeholder}
                                             </option>
-                                        ))}
-                                    </select>
-                                ) : field.type === "date" ? (
-                                    <DatePicker
-                                        selected={formData[field.name] || null}
-                                        onChange={(birthDate) =>
-                                            handleDateChange(
-                                                birthDate,
-                                                field.name
-                                            )
-                                        }
-                                        className="w-full p-2 border rounded-md shadow-xs hover:ring-1 hover:ring-primary focus:outline-hidden focus:ring-2 focus:ring-primary transition-all duration-300"
-                                        dateFormat="dd/MM/yyyy"
-                                        wrapperClassName="w-full"
-                                        showYearDropdown
-                                        showMonthDropdown
-                                        isClearable
-                                    />
-                                ) : (
-                                    <input
-                                        id={field.name}
-                                        name={field.name}
-                                        type={field.type}
-                                        placeholder={field.placeholder}
-                                        value={formData[field.name] || ""}
-                                        onChange={handleInputChange}
-                                        required={field.required}
-                                        className="w-full p-2 border rounded-[4px] shadow-xs hover:ring-1 hover:ring-primary focus:outline-hidden focus:ring-2 focus:ring-primary transition-all duration-300"
-                                    />
-                                )}
-                            </div>
-                        ))}
-                        <button
-                            type="button"
-                            className={`button-primary ${
-                                isLoading ? "opacity-50 cursor-not-allowed" : ""
-                            }`}
-                            onClick={handleAddData}
-                            disabled={isLoading}
-                        >
-                            Tambah
-                        </button>
+                                            {field.options.map((option) => (
+                                                <option
+                                                    key={option.value}
+                                                    value={option.value}
+                                                >
+                                                    {option.label}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    ) : field.type === "date" ? (
+                                        <DatePicker
+                                            selected={
+                                                formData[field.name] || null
+                                            }
+                                            onChange={(birthDate) =>
+                                                handleDateChange(
+                                                    birthDate,
+                                                    field.name
+                                                )
+                                            }
+                                            className="w-full p-2 border rounded-md shadow-xs hover:ring-1 hover:ring-primary focus:outline-hidden focus:ring-2 focus:ring-primary transition-all duration-300"
+                                            dateFormat="dd/MM/yyyy"
+                                            wrapperClassName="w-full"
+                                            showYearDropdown
+                                            showMonthDropdown
+                                            isClearable
+                                        />
+                                    ) : field.type === "textarea" ? (
+                                        <textarea
+                                            id={field.name}
+                                            name={field.name}
+                                            placeholder={field.placeholder}
+                                            value={formData[field.name] || ""}
+                                            onChange={handleInputChange}
+                                            required={field.required}
+                                            rows={3}
+                                            className="w-full p-2 border rounded-[4px] shadow-xs hover:ring-1 hover:ring-primary focus:outline-hidden focus:ring-2 focus:ring-primary transition-all duration-300"
+                                        />
+                                    ) : (
+                                        <input
+                                            id={field.name}
+                                            name={field.name}
+                                            type={field.type}
+                                            placeholder={field.placeholder}
+                                            value={formData[field.name] || ""}
+                                            onChange={handleInputChange}
+                                            required={field.required}
+                                            className="w-full p-2 border rounded-[4px] shadow-xs hover:ring-1 hover:ring-primary focus:outline-hidden focus:ring-2 focus:ring-primary transition-all duration-300"
+                                        />
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+                        <div className="flex justify-end">
+                            <button
+                                type="button"
+                                className={`button-primary ${
+                                    isLoading
+                                        ? "opacity-50 cursor-not-allowed"
+                                        : ""
+                                }`}
+                                onClick={handleAddData}
+                                disabled={isLoading}
+                            >
+                                Tambah
+                            </button>
+                        </div>
                     </form>
-                    {dataList.length > 0 && (
-                        <>
-                            <div>
-                                <h3 className="text-lg mt-1 font-normal">
-                                    List akun yang akan dibuat:
-                                </h3>
-                                <table className="min-w-full mt-4 border-collapse border border-gray-200">
+                </div>
+
+                {dataList.length > 0 && (
+                    <div className="card-basic rounded-md flex flex-col grow w-full lg:basis-3/5">
+                        <div>
+                            <h3 className="text-lg mt-1 font-normal">
+                                List akun yang akan dibuat:
+                            </h3>
+                            <div className="overflow-x-auto mt-4">
+                                <table className="min-w-full border-collapse border border-gray-200 text-sm">
                                     <thead>
-                                        <tr>
+                                        <tr className="bg-gray-50">
                                             <th className="border border-gray-200 p-2 text-left font-normal text-gray-500">
                                                 NAMA
                                             </th>
@@ -271,16 +369,19 @@ const RequestAccountForm = () => {
                                     </thead>
                                     <tbody>
                                         {dataList.map((data, index) => (
-                                            <tr key={index}>
-                                                <td className="border border-gray-200 p-2">
+                                            <tr
+                                                key={index}
+                                                className="odd:bg-white even:bg-gray-50"
+                                            >
+                                                <td className="border border-gray-200 p-2 whitespace-nowrap">
                                                     {data.name}
                                                 </td>
-                                                <td className="border border-gray-200 p-2">
+                                                <td className="border border-gray-200 p-2 whitespace-nowrap">
                                                     {formatDate(
                                                         data.dateOfBirth
                                                     )}
                                                 </td>
-                                                <td className="border border-gray-200 p-2">
+                                                <td className="border border-gray-200 p-2 whitespace-nowrap">
                                                     {isStudent
                                                         ? data.className
                                                         : data.email}
@@ -288,14 +389,15 @@ const RequestAccountForm = () => {
                                                 <td className="border border-gray-200 p-2">
                                                     <button
                                                         type="button"
-                                                        className="p-3 rounded-full text-gray-500 hover:bg-gray-200 hover:text-red-500 transition"
+                                                        className="p-2 rounded-full text-gray-500 hover:bg-gray-200 hover:text-red-500 transition"
                                                         onClick={() =>
                                                             handleDeleteData(
                                                                 index
                                                             )
                                                         }
+                                                        aria-label="Hapus"
                                                     >
-                                                        <Trash size={20} />
+                                                        <Trash size={18} />
                                                     </button>
                                                 </td>
                                             </tr>
@@ -303,6 +405,8 @@ const RequestAccountForm = () => {
                                     </tbody>
                                 </table>
                             </div>
+                        </div>
+                        <div className="mt-4">
                             <button
                                 type="button"
                                 className={`button-primary ${
@@ -319,11 +423,11 @@ const RequestAccountForm = () => {
                                     "Buat Pendaftaran Akun"
                                 )}
                             </button>
-                        </>
-                    )}
-                </div>
+                        </div>
+                    </div>
+                )}
             </div>
-        </>
+        </div>
     );
 };
 
